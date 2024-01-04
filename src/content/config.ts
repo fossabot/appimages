bc22a0507ceebe2ed65b4fc21f899e6a94e86aac
drsheppard01 <gbog@proton.me>
2024-01-04 19:33:58 +0300

    fix collections in config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,7 @@
 import { defineCollection, z } from "astro:content";
 
 // Post collection schema
-const blogCollection = defineCollection({
+const appsCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     meta_title: z.string().optional(),
@@ -51,7 +51,7 @@ const pagesCollection = defineCollection({
 
 // Export collections
 export const collections = {
-  blog: blogCollection,
+  apps: appsCollection,
   authors: authorsCollection,
   pages: pagesCollection,
 };
