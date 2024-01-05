97468caabbcbadb5254f5150edf81884b7743e52
DrSheppard <60893791+Drsheppard01@users.noreply.github.com>
2024-01-05 23:38:18 +0300

    update date format from astroplate

--- src/lib/utils/dateFormat.ts
+++ src/lib/utils/dateFormat.ts
@@ -1,10 +1,12 @@
-import { formatInTimeZone } from "date-fns-tz";
+import { format } from "date-fns";
 
 const dateFormat = (
   date: Date | string,
-  format: string = "dd MMM, yyyy"
+  pattern: string = "dd MMM, yyyy",
 ): string => {
-  return formatInTimeZone(date, "Europe/Moscow", format);
+  const dateObj = new Date(date);
+  const output = format(dateObj, pattern);
+  return output;
 };
 
 export default dateFormat;
